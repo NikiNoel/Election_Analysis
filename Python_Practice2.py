7c48c0cb77e35c4fc491bf86039a236175ced98f
Niki <NikiNoel1218@gmail.com>
2023-04-19 10:10:29 -0400

    Printing formats

--- Python_Practice2.py
+++ Python_Practice2.py
@@ -150,7 +150,7 @@ for voters in counties_dict.values():
     print(voters)
 for county, voters in counties_dict.items():
     print(county, voters)
-# Printing county and voter info into string
+# Skill Drill: Printing county and voter info into string
 for county, voters in counties_dict.items():
     print(county, "county has", voters, "registered voters")
 
@@ -167,3 +167,43 @@ for county_dict in voting_data:
 for county_dict in voting_data:
     for value in county_dict.values():
         print(value)
+
+
+# Editing code to use f-strings
+# # Original
+# my_votes = int(input("How many votes did you get in the election? "))
+# total_votes = int(input("What is the total votes in the election? "))
+# percentage_votes = (my_votes / total_votes) * 100
+# print("I received " + str(percentage_votes)+"% of the total votes.")
+
+# # f-strings
+# my_votes = int(input("How many votes did you get in the election? "))
+# total_votes = int(input("What is the total votes in the election? "))
+# print(f"I received {my_votes / total_votes * 100}% of the total votes.")
+
+
+counties_dict = {"Arapahoe": 369237, "Denver":413229, "Jefferson": 390222}
+for county, voters in counties_dict.items():
+    print(county + " county has " + str(voters) + " registered voters.")
+
+for county, voters in counties_dict.items():
+    print(f"{county} county has {voters} registered voters.")
+
+
+# Multiline f-strings
+candidate_votes = int(input("How many votes did the candidate get in the election? "))
+total_votes = int(input("What is the total number of votes in the election? "))
+# message_to_candidate = (
+    # f"You received {candidate_votes} number of votes. "
+    # f"The total number of votes in the election was {total_votes}. "
+    # f"You received {candidate_votes / total_votes * 100}% of the total votes.")
+# percentage formatted to 2 decimal places
+message_to_candidate = (
+    f"You received {candidate_votes:,} number of votes. "
+    f"The total number of votes in the election was {total_votes:,}. "
+    f"You received {candidate_votes / total_votes * 100:.2f}% of the total votes.")
+print(message_to_candidate)
+
+# format number in f-string:
+# f'{value:{width}.{precision}}'
+
