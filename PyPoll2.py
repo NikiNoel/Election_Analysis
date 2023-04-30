c70b63a328b97fb7555b6a0feea1b8e2cde7e988
Niki <NikiNoel1218@gmail.com>
2023-04-30 14:05:03 -0400

    Candidiate vote update

--- PyPoll2.py
+++ PyPoll2.py
@@ -17,6 +17,9 @@ file_to_save = os.path.join("Analysis", "election_analysis2.txt")
 # 1. Initialize a total vote counter.
 total_votes = 0
 
+# Candidate Options
+candidate_options = []
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
@@ -25,10 +28,19 @@ with open(file_to_load) as election_data:
     # Read the header row.
     headers = next(file_reader)
 
-    # Print each row in the CSV file.
+# Print each row in the CSV file.
     for row in file_reader:
-        # 2. Add to the total vote count.
-        total_votes += 1 
+        # Add to the total vote count.
+        total_votes += 1
 
-# 3. Print the total votes.
-print(total_votes)
+        # Print the candidate name from each row.
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            # Add it to the list of candidates.
+            candidate_options.append(candidate_name)
+
+# Print the candidate list.
+print(candidate_options)
+
